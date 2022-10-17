c9e0689b070300965d337c9b3334f0e8b972e409
そだ20 <jomin398@gmail.com>
2022-10-17 17:11:04 +0900

    update css and script

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import Rubify from './src/rubify/js/index.js';
 let chrStr = '天(てん)童(どう)ア()リ()ス()';
 let rr = new Rubify().complexConv(chrStr);
 window.onload = () => {
-    document.querySelector('.cont.r .mid ruby').innerHTML = rr;
+    const elmSelter = (s) => document.querySelector(s);
 
-    document.querySelector('form#edit .loc select').onchange = v => {
+    elmSelter('.cont.r .mid ruby').innerHTML = rr;
+    elmSelter('form#edit .loc select').onchange = v => {
         document.querySelector('form#edit .loc img').src = `./logos/${v.target.value == "Extra" ? "Schale" : v.target.value}.png`;
+    }
+    elmSelter('form#edit input[type="text"][name="name"]').onchange = v => {
+        document.querySelector('form#edit ruby.name.pre').innerHTML = new Rubify().complexConv(v.target.value);
     }
 }
