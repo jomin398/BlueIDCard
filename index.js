3cc43393ea4c2b9f1628a6fb7a859734ad2f4126
そだ20 <jomin398@gmail.com>
2022-10-18 17:33:50 +0900

    update form

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import Rubify from './src/rubify/js/index.js';
 let chrStr = '天(てん)童(どう)ア()リ()ス()';
 let rr = new Rubify().complexConv(chrStr);
+
+function genHex() {
+    var array = new Uint32Array(2);
+    let n = parseInt(window.crypto.getRandomValues(array), 16);
+    return parseInt(n.toString().slice(0, 11)).toString(16).toUpperCase();
+}
+
 window.onload = () => {
     const elmSelter = (s) => document.querySelector(s);
 
@@ -11,4 +18,14 @@ window.onload = () => {
     elmSelter('form#edit input[type="text"][name="name"]').onchange = v => {
         document.querySelector('form#edit ruby.name.pre').innerHTML = new Rubify().complexConv(v.target.value);
     }
+    elmSelter('.editForm a.modal-close').onclick = () => {
+        let d = new FormData(document.querySelector('form#edit'));
+        const value = Object.fromEntries(d.entries());
+        apply(value)
+    }
+}
+
+function apply(j) {
+    if (!j.id) j.id = genHex();
+    console.log(j)
 }
